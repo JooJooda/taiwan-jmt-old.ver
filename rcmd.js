63f79ce8555141f6351053be9e4293ac15223985
JooJooda <liyingzhou375@gmail.com>
2023-12-02 02:40:29 +0800

    food 페이지 html 수정 및 css

---
+++ rcmd.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".recmd", {
+  slidesPerView:2.5,
+  spaceBetween:10,
+  
+  scrollbar:{
+    el:".swiper-scrollbar",
+    draggable:false,
+    dragSize:80,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    clickable:true,
+  },
+
+  breakpoints:{
+    576:{
+      slidesPerView:3,
+      spaceBetween:15,
+    },
+    768:{
+      slidesPerView:4,
+      spaceBetween:20
+    },
+    1024:{
+      slidesPerView:5,
+      spaceBetween:30
+    }
+  },
+
+});
+
